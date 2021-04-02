d4303270d3353ad66fc4b733c82a717fdc99b760
Chernyshova_ae <gosevenday@mail.ru>
2021-04-02 19:08:39 +0300

    Урок 1, задание 6.

---
+++ Lesson-1 ex 6.py
@@ -0,0 +1,8 @@
+get_first_km = int(input('Введите результат пробежки (км.) в первый день:\n'))
+get_end_km = int(input('Введите целевой результат пробежки (км.):\n'))
+count_day = 1
+while get_first_km < get_end_km:
+    get_first_km +=  (get_first_km * 0.1)
+    count_day +=1
+    #print(get_first_km)
+print(f'На {count_day} день спортсмен достиг результата — не менее {get_end_km} км')
